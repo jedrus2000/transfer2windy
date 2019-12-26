cdad3754ba51a1a2971414ad73f2bc0ed8de2095
Andrzej Bargański <a.barganski@gmail.com>
2019-12-26 13:20:35 +0100

    Tests sample set of different stations

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,11 +4,11 @@ from transfer2windy.__main__ import lambda_handler
 
 class TestMain(unittest.TestCase):
     def test_lambda_handler(self):
-        stations = [
-            {"source_type": "polmil-metar", "source_id": "EPOK", "windy_station_id": ""},
-            {"source_type": "armaag", "source_id": "AM4", "windy_station_id": ""},
-            {"source_type": "armaag", "source_id": "AM11", "windy_station_id": ""},
-            {"source_type": "gddkia", "source_id": "145", "windy_station_id": ""}
-        ]
+        stations = [ { "source_type": "gddkia", "source_id": "145", "windy_station_id": "0" },
+                     { "source_type": "polmil-metar", "source_id": "EPOK","windy_station_id": "1" },
+                     { "source_type": "armaag", "source_id": "AM4", "windy_station_id": "2" },
+                     { "source_type": "armaag", "source_id": "AM11", "windy_station_id": "3" },
+                     { "source_type": "armaag", "source_id": "AM12", "windy_station_id": "4" },
+                     { "source_type": "iopan", "source_id": "SOPOT", "windy_station_id": "5" } ]
         print(lambda_handler(stations, None))
 
